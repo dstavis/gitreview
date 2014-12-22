3519c083f780ca4e7719fde9469da77e5562dd55
David Stavis <davidstavis@gmail.com>
2014-12-21 23:04:27 -0800

    User can successfully delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,10 +9,12 @@ class CommentsController < ApplicationController
     sha = params[:commit_id]
     commit = @client.get("https://api.github.com/repos/dstavis/gitreview/commits/#{sha}")
     success = @client.post(commit.comments_url, {body: params[:body]})
-    redirect_to commit_path(:commit_id)
+    redirect_to commit_path(params[:commit_id])
   end
 
   def destroy
-    
+    success = @client.delete("https://api.github.com/repos/dstavis/gitreview/comments/#{params[:id]}")
+    binding.pry
+    redirect_to commit_path(id: params[:commit_id])
   end
 end
